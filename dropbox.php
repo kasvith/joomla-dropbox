84d0daaf6d9ec2ce4746714433b3bcc5dbef8ccd
kasun <alankasun@gmail.com>
2017-06-27 21:22:42 +0530

    initil commit

--- dropbox.php
+++ dropbox.php
@@ -8,6 +8,8 @@
  * @link       http://your.url.com
  */
 
+use \Joomla\CMS\Plugin\CMSPlugin;
+
 defined('_JEXEC') or die;
 
 /**
@@ -16,114 +18,7 @@ defined('_JEXEC') or die;
  * @package  dropbox
  * @since    1.0
  */
-class plgSystemDropbox extends JPlugin
+class PlgFileSystemDropbox extends CMSPlugin
 {
-	/**
-	 * Application object
-	 *
-	 * @var    JApplicationCms
-	 * @since  1.0
-	 */
-	protected $app;
 
-	/**
-	 * Database object
-	 *
-	 * @var    JDatabaseDriver
-	 * @since  1.0
-	 */
-	protected $db;
-
-	/**
-	 * Affects constructor behavior. If true, language files will be loaded automatically.
-	 *
-	 * @var    boolean
-	 * @since  1.0
-	 */
-	protected $autoloadLanguage = true;
-
-	/**
-	 * onAfterInitialise.
-	 *
-	 * @return  void.
-	 *
-	 * @since   1.0
-	 */
-	public function onAfterInitialise()
-	{
-		
-	}
-
-	/**
-	 * onAfterRoute.
-	 *
-	 * @return  void.
-	 *
-	 * @since   1.0
-	 */
-	public function onAfterRoute()
-	{
-	
-	}
-
-	/**
-	 * onAfterDispatch.
-	 *
-	 * @return  void.
-	 *
-	 * @since   1.0
-	 */
-	public function onAfterDispatch()
-	{
-	
-	}
-
-	/**
-	 * onAfterRender.
-	 *
-	 * @return  void.
-	 *
-	 * @since   1.0
-	 */
-	public function onAfterRender()
-	{
-		// Access to plugin parameters
-		$sample = $this->params->get('sample', '42');
-	}
-
-	/**
-	 * onAfterCompileHead.
-	 *
-	 * @return  void.
-	 *
-	 * @since   1.0
-	 */
-	public function onAfterCompileHead()
-	{
-	
-	}
-
-	/**
-	 * OnAfterCompress.
-	 *
-	 * @return  void.
-	 *
-	 * @since   1.0
-	 */
-	public function onAfterCompress()
-	{
-	
-	}
-
-	/**
-	 * onAfterRespond.
-	 *
-	 * @return  void.
-	 *
-	 * @since   1.0
-	 */
-	public function onAfterRespond()
-	{
-	
-	}
 }
